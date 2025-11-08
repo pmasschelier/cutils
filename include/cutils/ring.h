7201a337847769f14d0aa7024efd00f0b049eb1b
PM <pmasschelier@mailo.com>
2025-11-08 18:01:41 -0300

    rings now store their length

--- include/cutils/ring.h
+++ include/cutils/ring.h
@@ -9,38 +9,57 @@
 #include <string.h>
 #endif
 
-#define ring_empty(ring) (!(ring).full && ((ring).begin == (ring).next))
+#define ring_empty(ring) ((ring).length == 0)
+#define ring_full(ring) ((ring).length == (ring).capacity)
 #define ring_next(ring, index) (((index) + 1) % (ring).capacity)
-#define ring_prev(ring, index) (((index) - 1) % (ring).capacity)
-#define ring_count(ring) ((ring).full ? (ring).capacity : (ring).next - (ring).begin)
-#define ring_head(ring, count) ((ring).next = ((ring).begin + MIN(count, ring_count(ring))) % (ring).capacity)
-#define ring_tail(ring, count) ((ring).begin = ((ring).next - MIN(count, ring_count(ring))) % (ring).capacity)
+#define ring_prev(ring, index) (((index) + (ring).capacity - 1) % (ring).capacity)
+#define ring_get_unsafe(ring, index) (&(ring).data[((ring).begin + (index)) % (ring).capacity])
+#define ring_get(ring, index) ((index) < (ring).length ? ring_get_unsafe(ring, index) : NULL)
+#define ring_init(type, buffer, size, free_element) (type ## _ring_t) { .data = (type*) buffer, .capacity = size, .free = free_element }
+#define ring_foreach(ring, e, type)                                                     \
+    for(type* e = ring_get(ring, 0); e != NULL; e = NULL)                               \
+        for(unsigned index = 0; index != (ring).length && (e = ring_get_unsafe(ring, index)); index++)
 
-#define DEFINE_RING_TYPE(type)                                                  \
+#define DEFINE_RING_TYPE(type)              \
+    DEFINE_INTERFACE_RING_TYPE(type)        \
+    DEFINE_IMPLEMENTATION_RING_TYPE(type)   \
+
+#define DEFINE_INTERFACE_RING_TYPE(type)                                        \
     typedef struct {                                                            \
         type* data;                                                             \
+        void (*free)(type*);                                                    \
         unsigned capacity;                                                      \
         unsigned begin;                                                         \
         unsigned next;                                                          \
-        bool full;                                                              \
+        unsigned length;                                                        \
     } type ## _ring_t;                                                          \
-                                                                                \
-    UNUSED static type ## _ring_t ring_init_ ## type(                 \
-        unsigned capacity                                                       \
+
+#define DEFINE_IMPLEMENTATION_RING_TYPE(type)                                   \
+    UNUSED static type ## _ring_t ring_create_ ## type(                         \
+        unsigned capacity, void (*free_element)(type*)                          \
     ) {                                                                         \
-        assert(capacity > 0);                                                   \
+        when_false_ret(capacity > 0, (type ## _ring_t) { .capacity = 0 });      \
         return (type ## _ring_t) {                                              \
             .data = CUTILS_alloc(capacity * sizeof(type)),                      \
             .capacity = capacity,                                               \
+            .free = free_element                                                \
         };                                                                      \
     }                                                                           \
                                                                                 \
-    UNUSED static void ring_deinit_ ## type(type ## _ring_t* ring) {  \
+    UNUSED static void ring_free_ ## type(type ## _ring_t* ring) {              \
+        if(ring->free) {                                                        \
+            ring_foreach(*ring, e, type) {                                      \
+                ring->free(e);                                                  \
+            }                                                                   \
+        }                                                                       \
         CUTILS_dealloc(ring->data);                                             \
-        ring->capacity = 0;                                                     \
-        ring->begin = 0;                                                        \
-        ring->next = 0;                                                         \
-        ring->full = false;                                                     \
+        *ring = (type ## _ring_t) {                                             \
+            .data = NULL,                                                       \
+            .capacity = 0,                                                      \
+            .begin = 0,                                                         \
+            .next = 0,                                                          \
+            .length = 0,                                                        \
+        };                                                                      \
     }                                                                           \
                                                                                 \
     UNUSED static type* ring_front_ ## type(type ## _ring_t* ring) {            \
@@ -49,28 +68,61 @@
         return &ring->data[ring->begin];                                        \
     }                                                                           \
                                                                                 \
+    UNUSED static type* ring_back_ ## type(type ## _ring_t* ring) {             \
+        if(ring_empty(*ring))                                                   \
+            return NULL;                                                        \
+        unsigned last = ring_prev(*ring, ring->next);                           \
+        return &ring->data[last];                                               \
+    }                                                                           \
+                                                                                \
+    UNUSED static unsigned ring_head_ ## type(                                  \
+        type ## _ring_t* ring, unsigned count                                   \
+    ) {                                                                         \
+        if(ring_empty(*ring) || count >= ring->length) return ring->length;     \
+        if (ring->free) {                                                       \
+            for(unsigned index = count; index < ring->length; index++)          \
+                ring->free(ring_get_unsafe(*ring, index));                      \
+        }                                                                       \
+        ring->length = count;                                                   \
+        ring->next = (ring->begin + count) % ring->capacity;                    \
+        return count;                                                           \
+    }                                                                           \
+                                                                                \
+    UNUSED static unsigned ring_tail_ ## type(                                  \
+        type ## _ring_t* ring, unsigned count                                   \
+    ) {                                                                         \
+        if(ring_empty(*ring) || count >= ring->length) return ring->length;     \
+        if (ring->free && !ring_empty(*ring)) {                                 \
+            for(unsigned index = 0; index < ring->length - count; index++)      \
+                ring->free(ring_get_unsafe(*ring, index));                      \
+        }                                                                       \
+        ring->length = count;                                                   \
+        ring->begin = (ring->capacity + ring->next - count) % ring->capacity;   \
+        return count;                                                           \
+    }                                                                           \
+                                                                                \
     UNUSED static bool ring_pop_back_ ## type(                                  \
-        type ## _ring_t* ring, type* popped                                     \
+        type ## _ring_t* ring                                                   \
     ) {                                                                         \
         if(ring_empty(*ring))                                                   \
             return false;                                                       \
         unsigned last = ring_prev(*ring, ring->next);                           \
-        if(popped != NULL)                                                      \
-            memcpy(popped, &ring->data[last], sizeof(type));                    \
+        if (ring->free)                                                         \
+            ring->free(&ring->data[last]);                                      \
         ring->next = last;                                                      \
-        ring->full = false;                                                     \
+        ring->length -= 1;                                                      \
         return true;                                                            \
     }                                                                           \
                                                                                 \
     UNUSED static bool ring_pop_front_ ## type(                                 \
-        type ## _ring_t* ring, type* popped                                     \
+        type ## _ring_t* ring                                                   \
     ) {                                                                         \
         if(ring_empty(*ring))                                                   \
             return false;                                                       \
-        if(popped != NULL)                                                      \
-            memcpy(popped, &ring->data[ring->begin], sizeof(type));             \
+        if (ring->free)                                                         \
+            ring->free(&ring->data[ring->begin]);                               \
         ring->begin = ring_next(*ring, ring->begin);                            \
-        ring->full = false;                                                     \
+        ring->length -= 1;                                                      \
         return true;                                                            \
     }                                                                           \
                                                                                 \
@@ -78,12 +130,14 @@
         type ## _ring_t* ring, bool force                                       \
     ) {                                                                         \
         const unsigned next = ring_next(*ring, ring->next);                     \
-        if(ring->full) {                                                        \
+        if(ring_full(*ring)) {                                                  \
             if (!force)                                                         \
                 return NULL;                                                    \
+            if (ring->free)                                                     \
+                ring->free(&ring->data[ring->begin]);                           \
             ring->begin = ring_next(*ring, ring->begin);                        \
-        } else if(next == ring->begin) {                                        \
-            ring->full = true;                                                  \
+        } else {                                                                \
+            ring->length += 1;                                                  \
         }                                                                       \
         const unsigned pop = ring->next;                                        \
         ring->next = next;                                                      \
